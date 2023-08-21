c2e8af1a149d2cd80e76371dec7308f1c2bfbf5d
SammyTBrains <josephsamuel034@gmail.com>
2023-08-22 00:45:17 +0100

    hs

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 type Props = { userNumber: number };
 
@@ -27,14 +28,57 @@ const generateRandomBetween = (
   }
 };
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 const GameScreen = (props: Props) => {
-  const initialGuess = generateRandomBetween(1, 100, props.userNumber);
+  const initialGuess = generateRandomBetween(
+    minBoundary,
+    maxBoundary,
+    props.userNumber
+  );
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  const nextGuessHandler = (direction: string) => {
+    if (
+      (direction === "lower" && currentGuess < props.userNumber) ||
+      (direction === "greater" && currentGuess > props.userNumber)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+    switch (direction) {
+      case "lower":
+        maxBoundary = currentGuess;
+        break;
+      case "greater":
+        minBoundary = currentGuess + 1;
+        break;
+    }
+
+    const newRandNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+
+    setCurrentGuess(newRandNumber);
+  };
 
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+          -
+        </PrimaryButton>
+        <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+          +
+        </PrimaryButton>
+      </View>
     </View>
   );
 };
