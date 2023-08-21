4eb7e2a1677444551dcd1c2a6acf649247599fee
SammyTBrains <josephsamuel034@gmail.com>
2023-08-21 23:00:25 +0100

    background image

--- App.tsx
+++ App.tsx
@@ -1,9 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#3f0337", "#B8BF1D"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/GuessingGameBG.jpg")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
