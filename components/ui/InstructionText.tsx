6509b19995fef7c7b1bc3f962d989e5e067b3efb
SammyTBrains <josephsamuel034@gmail.com>
2023-08-22 20:04:18 +0100

    cascading

--- components/ui/InstructionText.tsx
+++ components/ui/InstructionText.tsx
@@ -2,10 +2,13 @@ import { ReactNode } from "react";
 import { StyleSheet, Text } from "react-native";
 import Colors from "../../constants/color";
 
-type Props = { children: ReactNode };
+type Props = { children: ReactNode; style?: {} };
 
 const InstructionText = (props: Props) => {
-  return <Text style={styles.instructionText}>{props.children}</Text>;
+  return (
+    //props.style on the right means we want incoming style to be able to override default style
+    <Text style={[styles.instructionText, props.style]}>{props.children}</Text>
+  );
 };
 
 export default InstructionText;
